f123a2a95b2abb61bbea767b663b322412e03e87
Sheraz <mrsheraz325@gmail.com>
2026-05-02 21:01:12 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: [
-    "https://gastimation.vercel.app",
-    "http://localhost:5173",
-    "https://gastimation.vercel.app"
+   "https://gastimation-frontend.vercel.app",
+    "http://localhost:5173"
   ],
   credentials: true
 }));
